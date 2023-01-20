3051ee2ee444f38189b22e3b94c7d9f54e446156
Arlequiin <78297845+Arlequiin@users.noreply.github.com>
2023-01-20 15:57:31 +0100

    fonction

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 rows=[]
-out=[]
 with open("dico.txt") as f:
     for row in f:
         rows.append(row.strip())
+def indice_mot(word):
+  return ''.join(sorted(word))
+def is_anagram(mot1,mot2):
+    return indice_mot(mot1)==indice_mot(mot2)
 def dictionnaire_indices_mots(liste):
   d={}
   for element in liste:
     l=[]
     for element2 in liste:
-      if sorted(element2)==sorted(element):
+      if is_anagram(element,element2):
         l.append(element2)
-    element=''.join(sorted(element))
+    element=indice_mot(element)
     d[element]=l
   return d
 print(dictionnaire_indices_mots(rows))
+
